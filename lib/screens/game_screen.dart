e89208f1be84e596769b8cc82d51ca22b78164fd
JayWoodroffe <jenawoodroffe@gmail.com>
2025-04-10 11:46:57 +0200

    word updates
    reducing lives for wrong guesses, changing the color of guessed letters, changing the displayed word as correct letters are guessed

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -13,23 +15,28 @@ class GameScreen extends StatefulWidget {
 class _GameScreenState extends State<GameScreen> {
   int lives = 7;
 
-  //randomly selecting a word from the list
+  List guessedLetters = [];
+
+  //randomly selecting a word from the list upon running
   String word = wordsList[Random().nextInt(wordsList.length)];
 
   //generate row of hearts
   Widget _buildLivesDisplay() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(
-        lives,
-        (index) => const Padding(
-          padding: EdgeInsets.only(
-            left: 15,
-          ),
-          child: Image(
-            width: 30,
-            height: 20,
-            image: AssetImage("resources/images/heart.png"),
+    return Container(
+      margin: EdgeInsets.only(left: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: List.generate(
+          lives,
+          (index) => const Padding(
+            padding: EdgeInsets.only(
+              left: 10,
+            ),
+            child: Image(
+              width: 30,
+              height: 20,
+              image: AssetImage("resources/images/heart.png"),
+            ),
           ),
         ),
       ),
@@ -38,10 +45,11 @@ class _GameScreenState extends State<GameScreen> {
 
   //managing how the semi-guessed word is displayed
   String _handleText() {
-    String displayedWord = "";
-    List guessedLetters = [];
+    String displayedWord = " ";
+
     for (int i = 0; i < word.length; i++) {
       String char = word[i];
+      //if the current character has been guessed already, it will be included in the guessedLetters list and can be displayed
       if (guessedLetters.contains(char)) {
         displayedWord += "$char ";
       } else {
@@ -49,6 +57,36 @@ class _GameScreenState extends State<GameScreen> {
       }
     }
     return displayedWord;
+  }
+
+  _checkLetter(String letter) {
+    if (word.contains(letter)) {
+      setState(() {
+        guessedLetters.add(letter);
+      });
+    } else {
+      if (lives >= 1) {
+        setState(() {
+          guessedLetters.add(letter);
+          lives--;
+        });
+      } else {
+        print("You lost");
+      }
+    }
+    bool wordCompleted = true;
+    for (int i = 0; i < word.length; i++) {
+      String char = word[i];
+      if (!guessedLetters.contains(char)) {
+        setState(() {
+          wordCompleted = false;
+        });
+        break;
+      }
+      if (wordCompleted) {
+        print("Won");
+      }
+    }
   }
 
   @override
@@ -85,25 +123,30 @@ class _GameScreenState extends State<GameScreen> {
           child: Column(
             children: [
               //points display
-              Container(
-                decoration: BoxDecoration(color: Colors.red.shade900),
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(top: 20),
-                width: MediaQuery.of(context).size.width / 3.5,
-                height: 35,
-                child: Center(
-                    child: Text("12 points",
-                        style: gothicTextStyle(
-                            24, Colors.black, FontWeight.w300))),
-              ),
+              //TODO: decide if points even make sense(tutorial gives 5 points for each correctly guessed letter??)
+              // Align(
+              //   alignment: Alignment.centerLeft,
+              //   child: Container(
+              //     decoration: BoxDecoration(
+              //         color: Colors.red.shade900,
+              //         borderRadius: BorderRadius.circular(15)),
+              //     margin: EdgeInsets.only(top: 20, left: 30),
+              //     width: MediaQuery.of(context).size.width / 3.5,
+              //     height: 35,
+              //     child: Center(
+              //         child: Text("12 points",
+              //             style: gothicTextStyle(
+              //                 24, Colors.black, FontWeight.w300))),
+              //   ),
+              // ),
 
-              SizedBox(height: 20),
+              SizedBox(height: 40),
 
               //hangman image
               Image(
-                width: 155,
-                height: 155,
-                image: AssetImage("resources/images/hangman0.png"),
+                width: 180,
+                height: 180,
+                image: AssetImage(images[7 - lives]),
                 fit: BoxFit.cover,
                 color: Colors.white,
               ),
@@ -118,9 +161,16 @@ class _GameScreenState extends State<GameScreen> {
               SizedBox(height: 10),
 
               //the word
-              Text(
-                _handleText(),
-                style: gothicTextStyle(45, Colors.white),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(color: Colors.red.shade900, width: 3)),
+                child: Text(
+                  _handleText(),
+                  style: gothicTextStyle(45, Colors.grey[200]),
+                ),
               ),
 
               //keyboard
@@ -131,12 +181,19 @@ class _GameScreenState extends State<GameScreen> {
                 physics: NeverScrollableScrollPhysics(),
                 children: letters.map((alphabet) {
                   return InkWell(
+                    onTap: guessedLetters.contains(alphabet)
+                        ? null
+                        : () => _checkLetter(alphabet),
                     child: Center(
                       child: Padding(
                         padding: const EdgeInsets.only(bottom: 8.0),
                         child: Text(
                           alphabet,
-                          style: gothicTextStyle(35, Colors.white),
+                          style: gothicTextStyle(
+                              35,
+                              guessedLetters.contains(alphabet)
+                                  ? Colors.red.shade900
+                                  : Colors.white),
                         ),
                       ),
                     ),
